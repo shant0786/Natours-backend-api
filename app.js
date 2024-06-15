3c8426bed716147c11b02d9a2f892c440e6de664
shant0786 <rm.shanto786@gmail.com>
2024-06-15 20:28:03 +0600

    After video 114 {appError.js and errorController are created for better Error Handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -28,25 +31,14 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on server!`
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server !`);
-  err.status = 'fail';
-  err.statusCode = 400;
-  next(err);
+  // const err = new Error(`Can't find ${req.originalUrl} on this server !`);
+  // err.status = 'fail';
+  // err.statusCode = 400;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 ///* START SERVER *\\\
 
