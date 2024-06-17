e8332750c93a488fb5793fc0e8368b873a3b8cfb
shant0786 <rm.shanto786@gmail.com>
2024-06-17 10:33:52 +0600

    After Video 118/ Separating Production Error and Development Error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -31,7 +31,7 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'devlopment') {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     sendErrorProd(err, res);
