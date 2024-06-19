3bd4909e9d297105e532c5e331ed566e49dd6aad
shant0786 <rm.shanto786@gmail.com>
2024-06-19 12:29:10 +0600

    After video 121 bug fixed production error separation and meaning full error message to production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,6 +10,11 @@ const handleDuplicateFieldsDB = err => {
   const message = `Duplicate field value:${value}. Please use another value!`;
   return new AppError(message, 400);
 };
+const hanleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -50,7 +55,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-
+    if (error.name === 'ValidationError') error = hanleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
