82bec130433bff391bcaba9764ebe00a976b5b47
shant0786 <rm.shanto786@gmail.com>
2024-06-30 22:37:42 +0600

    After video 128/ JWT token generation and signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,23 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body); // User.save to CREATE and UPDATE
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.Confirm
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+  // User.save to CREATE and UPDATE
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
