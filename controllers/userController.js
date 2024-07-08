438b130e43754f9cbfe9c5b7dbcafc0b78f21897
shant0786 <rm.shanto786@gmail.com>
2024-07-08 14:29:22 +0600

    After Video-139/feat:Update only user data filtering out important fields fix:none

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,15 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowesFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach(el => {
+    if (allowesFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -15,21 +24,31 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
-      AppError(
+      new AppError(
         'This route is not for password update.please use /updateMyPassword.',
         400
       )
     );
   }
-  // 2) Update user document
-  res.status(200).json({
-    status: 'success'
+  // 2) Filtered out unwanted filed names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
   });
-};
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
 
 exports.createUser = (req, res) => {
   res
