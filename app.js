0641227659aa5011e9040a55a4b19a8e9861b686
shant0786 <rm.shanto786@gmail.com>
2024-07-10 21:58:37 +0600

    After video-146/fix:Parameter pollution using (hpp)http parameter polution

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -36,6 +37,19 @@ app.use(mongoSanitize());
 
 // Data Sanitization against XSS
 app.use(xss());
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 // Serving statics files
 app.use(express.static(`${__dirname}/public`));
 
