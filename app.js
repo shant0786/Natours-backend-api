83d3d24ac5d1cb0a34ee3a1643f64bbe12582e06
shant0786 <rm.shanto786@gmail.com>
2024-07-10 21:44:16 +0600

    After video-145/feat:Data sanitization using (mongoSanitizer) and SQL injection prevention,html javascript injection sanitization using (xss)

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,12 +31,18 @@ app.use('/api', limiter);
 // Body parcer, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 // Serving statics files
 app.use(express.static(`${__dirname}/public`));
 
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+
   // console.log(req.headers);
   next();
 });
