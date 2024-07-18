405ea33ef58551275e0107588dcf477c0d4d442b
shant0786 <rm.shanto786@gmail.com>
2024-07-18 18:14:25 +0600

    After video-151/Embeding data in a document by id

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const slugify = require('slugify');
+const User = require('./userModel');
 
 // const validator = require('validator');
 
@@ -102,7 +103,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -118,8 +120,14 @@ tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 // tourSchema.pre('save', function(next) {
-//   console.log('Will save document...');
+//  console.log('Will save document...');
 //   next();
 // });
 // tourSchema.post('save', function(doc, next) {
