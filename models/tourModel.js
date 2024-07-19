025d0dc08e2f31abd47cab74f5c0c4a0659041c7
shant0786 <rm.shanto786@gmail.com>
2024-07-19 11:05:16 +0600

    After video-152/Modeling tour guides in child referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const slugify = require('slugify');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 // const validator = require('validator');
 
@@ -104,7 +104,7 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    guides: Array
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
   },
   {
     toJSON: { virtuals: true },
@@ -121,11 +121,12 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
 // tourSchema.pre('save', function(next) {
 //  console.log('Will save document...');
 //   next();
