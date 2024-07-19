da36427e100a72e11f020f407cbfb4f26af6a4ed
shant0786 <rm.shanto786@gmail.com>
2024-07-19 11:55:19 +0600

    After video-156/Populating Reviews to populate to field u must use populate twise

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,16 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function() {
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
