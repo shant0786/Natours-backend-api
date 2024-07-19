9b9fb630822f7a1e8f41fb3a499490cc467528e7
shant0786 <rm.shanto786@gmail.com>
2024-07-19 11:27:58 +0600

    After video-153/Populating or realtime referencing data by qureymiddleware,expression,dry

--- models/tourModel.js
+++ models/tourModel.js
@@ -137,11 +137,19 @@ tourSchema.pre('save', function(next) {
 // });
 
 // QUERY MIDDLEWARE
+
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
+});
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
   next();
 });
+
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
