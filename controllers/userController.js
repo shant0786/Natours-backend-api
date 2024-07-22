e6335f0fd9d89ed95859d23a09e3ba14a1441f34
shant0786 <rm.shanto786@gmail.com>
2024-07-22 21:57:23 +0600

    After video-162/Update create factory handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,9 +71,6 @@ exports.createUser = (req, res) => {
     .json({ status: 'error', message: 'This route is not yet defined!' });
 };
 
-exports.updateUser = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined!' });
-};
+// Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
