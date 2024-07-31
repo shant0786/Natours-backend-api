f7181047907883ed9c98d8a20991f0a1bde7e38c
shant0786 <rm.shanto786@gmail.com>
2024-07-31 14:55:27 +0600

    After video-177/feat:Learning pug syntax

--- app.js
+++ app.js
@@ -67,7 +67,10 @@ app.use((req, res, next) => {
 });
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hicker',
+    user: 'Jonas'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
