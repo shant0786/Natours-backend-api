e1ae8bdea0b7da13a760a5f92c43be73f3d39746
shant0786 <rm.shanto786@gmail.com>
2024-09-07 17:13:31 +0600

    After video-195

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,12 +1,14 @@
-const catchAsync = require('../utils/catchAsync');
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getOverview = catchAsync(async (req, res) => {
-  //  1) get tour data from cllection
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
   const tours = await Tour.find();
 
   // 2) Build template
-  // 3)Render that template using tour data from 1
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
     tours
@@ -14,26 +16,22 @@ exports.getOverview = catchAsync(async (req, res) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  // 1) get the data , for the requested tour (including review and guides)
-
+  // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user'
   });
-  //2) buid template
 
-  // render template
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
 
-  res
-    .status(200)
-    .set(
-      'Content-Security-Policy',
-      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    )
-    .render('tour', {
-      title: `${tour.name} Tour`,
-      tour
-    });
+  // 2) Build template
+  // 3) Render template using data from 1)
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour
+  });
 });
 
 exports.getLoginForm = (req, res) => {
@@ -41,3 +39,28 @@ exports.getLoginForm = (req, res) => {
     title: 'Log into your account'
   });
 };
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser
+  });
+});
