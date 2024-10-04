188ecad2323e29aa50b6fbb0549e41e68334f3a7
shant0786 <rm.shanto786@gmail.com>
2024-10-05 00:29:46 +0600

    After video-201/impv:creating name for uploaded img and showing in page

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,28 +5,28 @@ const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-const multerStorages = multer.diskStorage({
+const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'public/img/users');
-  },{
-    filename: (req, file, cb) => {
-    const ext=file.mimetype.split("/")[1];
+  },
+
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
   }
 });
 
-const multerFilter=(req, file, cb) => {
-  if(file.mimetype.startsWith("image")) {
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
     cb(null, true);
   } else {
     cb(new AppError('Not an image! Please upload only images.', 400), false);
-  
-}
-}
+  }
+};
 const upload = multer({
-  storage:multerStorage,filters:multerFilter
+  storage: multerStorage,
+  filters: multerFilter
 });
-
 
 exports.uploadUserPhoto = upload.single('photo');
 const filterObj = (obj, ...allowesFields) => {
@@ -44,8 +44,6 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -57,6 +55,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   // 2) Filtered out unwanted filed names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
