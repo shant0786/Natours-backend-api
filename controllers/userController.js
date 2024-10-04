f9efec7d9c68cc8ee2586c6d4066b16031259f07
shant0786 <rm.shanto786@gmail.com>
2024-10-04 18:40:00 +0600

    After video-200/setting file name for the upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,34 @@
+const multer = require('multer');
+
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
+const multerStorages = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },{
+    filename: (req, file, cb) => {
+    const ext=file.mimetype.split("/")[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  }
+});
+
+const multerFilter=(req, file, cb) => {
+  if(file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  
+}
+}
+const upload = multer({
+  storage:multerStorage,filters:multerFilter
+});
+
+
+exports.uploadUserPhoto = upload.single('photo');
 const filterObj = (obj, ...allowesFields) => {
   const newObj = {};
 
